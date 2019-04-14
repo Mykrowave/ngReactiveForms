678056bb4b060b349aec6e1ff045064217ad0b8c
Mike Parker <mikeparker@crescerance.com>
2019-04-14 17:31:42 -0400

    rangeValidator

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Customer } from './customer';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-customer',
@@ -18,7 +18,10 @@ export class CustomerComponent implements OnInit {
     this.customerForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
-      email: ['', Validators.required, Validators.email],
+      email: ['', [Validators.required, Validators.email]],
+      phone: '',
+      rating: [null, [rangeWholeNumber]],
+      sendNotification: 'email',
       sendCatalog: true,
     });
   }
@@ -35,4 +38,29 @@ export class CustomerComponent implements OnInit {
       email: 'mikeparkercrescerance.com'
     });
   }
+
+  sendNotificationRadioClicked(): void {
+    console.log('value: ' + this.customerForm.get('sendNotification').value);
+
+    if (this.customerForm.get('sendNotification').value === 'text') {
+      this.customerForm.get('phone').setValidators(Validators.required);
+    } else {
+      this.customerForm.get('phone').clearValidators();
+    }
+
+    this.customerForm.get('phone').updateValueAndValidity();
+
+  }
 }
+
+
+  export function rangeWholeNumber(control: AbstractControl): {[key: string]: boolean} | null {
+    if (control.value > 0 &&
+        control.value < 6 && Math.round(+control.value) === +control.value) {
+
+    } else {
+      return {'range': true};
+    }
+
+    return null;
+  }
